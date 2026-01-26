277e39e6175f5bc9bb0df91af2c3431aaf35f6f5
Jeff Kwok <jeffjkwok@gmail.com>
2026-01-25 23:39:15 -0800

    add cards endpoint

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,6 +13,29 @@ app.get('/api/health', (req, res) => {
     });
 })
 
+app.get('/api/cards/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const response = await fetch(
+            `https://api.pokemontcg.io/v2/cards/${id}`
+        )
+
+        if (!response.ok) {
+            return res.status(response.status).json({
+                error: 'Card not found'
+            });
+        }
+
+        const data = await response.json();
+        res.json(data.data);
+
+    } catch (err) {
+        console.error('Erro fetching card:', err);
+        res.status(500).json({error: 'Failed to fetch cardF'})
+    }
+});
+
 app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`)
+    console.log(`Server running on http://localhost:${PORT}`)
 })
