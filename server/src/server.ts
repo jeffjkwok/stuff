8948efc608795621bc75d5cd5e9016f947bcad68
Jeff Kwok <jeffjkwok@gmail.com>
2026-01-26 02:26:49 -0800

    Script to fetch WOTC cards when API is back up; created mock card data and moved it into own directory

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import 'dotenv/config'
+import mockCards from './data/mock-cards.json'
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
+
+const cardMap = new Map(mockCards.map((card: any) => [card.id, card]))
 
 app.get('/api/health', (req, res) => {
     res.json({
@@ -14,65 +17,19 @@ app.get('/api/health', (req, res) => {
     });
 })
 
-const mockCards: Record<string, any> = {
-    'base1-4': {
-        id: 'base1-4',
-        name: 'Charizard',
-        nationalPokedexNumbers: [6],
-        images: {
-            small: 'https://images.pokemontcg.io/base1/4.png',
-            large: 'https://images.pokemontcg.io/base1/4_hires.png'
-        },
-        hp: '120',
-        types: ['Fire'],
-        set: {
-            id: 'base1',
-            name: 'Base',
-            series: 'Base'
-        },
-        rarity: 'Rare Holo'
-    },
-    'base1-1': {
-        id: 'base1-1',
-        name: 'Alakazam',
-        nationalPokedexNumbers: [65],
-        images: {
-            small: 'https://images.pokemontcg.io/base1/1.png',
-            large: 'https://images.pokemontcg.io/base1/1_hires.png'
-        },
-        hp: '80',
-        types: ['Psychic'],
-        set: {
-            id: 'base1',
-            name: 'Base',
-            series: 'Base'
-        },
-        rarity: 'Rare Holo'
-    },
-    'xy1-1': {
-        id: 'xy1-1',
-        name: 'Venusaur-EX',
-        nationalPokedexNumbers: [3],
-        images: {
-            small: 'https://images.pokemontcg.io/xy1/1.png',
-            large: 'https://images.pokemontcg.io/xy1/1_hires.png'
-        },
-        hp: '180',
-        types: ['Grass'],
-        set: {
-            id: 'xy1',
-            name: 'XY',
-            series: 'XY'
-        },
-        rarity: 'Rare Holo EX'
-    }
-};
-
 app.get('/api/cards/:id', async (req, res) => {
     try {
         const { id } = req.params;
 
         await new Promise(resolve => setTimeout(resolve, 500))
+
+        const card = cardMap.get(id)
+
+        if (card) {
+            res.json(card);
+        } else {
+            res.status(404).json({ error: 'Card not found' })
+        }
 
         // const response = await fetch(
         //     `https://api.pokemontcg.io/v2/cards/${id}`,
@@ -82,12 +39,6 @@ app.get('/api/cards/:id', async (req, res) => {
         //         }
         //     }
         // )
-
-        if (mockCards[id]) {
-            return res.json(mockCards[id]);
-        }
-
-        res.status(404).json({ error: 'Card not found' });
 
         // if (!response.ok) {
         //     return res.status(response.status).json({
@@ -106,4 +57,5 @@ app.get('/api/cards/:id', async (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
+    console.log(`Mock data loaded! `)
 })
