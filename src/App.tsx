15897e944c514992f8a8be3b16c469b835023757
Jeff Kwok <jeffjkwok@gmail.com>
2026-01-25 16:24:44 -0800

    Initial commit; Project Structure Vite React with SCSS

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import styles from './App.module.scss';
+
+function App() {
+
+  return (
+    <>
+      <div className={styles.container}>
+        <h1>Pokedex Tracker</h1>
+        <p>Ready to build!</p>
+      </div>
+    </>
+  )
+}
+
+export default App
