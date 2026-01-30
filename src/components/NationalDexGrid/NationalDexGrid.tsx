215d70fb74222020f4ca0a01171c84d5d3cef0d0
Jeff Kwok <jeffjkwok@gmail.com>
2026-01-30 01:57:20 -0800

    Create Grid Item and use dynamic logic for Clip Path; still need to solve improper loading

--- src/components/NationalDexGrid/NationalDexGrid.tsx
+++ src/components/NationalDexGrid/NationalDexGrid.tsx
@@ -1,9 +1,10 @@
 import { useState, useEffect, useMemo } from "react";
 import NationalDexFilters from "../NationalDexFilters/NationalDexFilters";
+import NationalDexGridItem from "../NationalDexGridItem/NationalDexGridItem";
 import type { FilterState } from "../NationalDexFilters/NationalDexFilters";
 import styles from "../NationalDexGrid/NationalDexGrid.module.scss";
 
-interface Pokemon {
+export interface Pokemon {
   id: string;
   name: string;
   generation: number;
@@ -61,26 +62,27 @@ export default function NationalDexGrid() {
         filteredCount={filteredPokemon.length}
       />
       {filteredPokemon.map((pokemon) => (
-        <div
-          key={pokemon.id}
-          className={styles.card}
-          onClick={() =>
-            console.log(`This is #${pokemon.id}, ${pokemon.name} `)
-          }
-        >
-          {/* <div className={styles.cardInfo} style={{ "filter": "grayscale(100%)" }}> */}
-          <div className={styles.cardInfo}>
-            <h3 className={styles.cardName}>{pokemon.name}</h3>
-            <p className={styles.cardSet}>#{pokemon.id}</p>
-            <div className={styles.placeholderImageWrapper}>
-              <img
-                className={styles.placeholderImage}
-                src={pokemon.originalArtwork}
-                alt=""
-              />
-            </div>
-          </div>
-        </div>
+        // <div
+        //   key={pokemon.id}
+        //   className={styles.card}
+        //   onClick={() =>
+        //     console.log(`This is #${pokemon.id}, ${pokemon.name} `)
+        //   }
+        // >
+        //   {/* <div className={styles.cardInfo} style={{ "filter": "grayscale(100%)" }}> */}
+        //   <div className={styles.cardInfo}>
+        //     <h3 className={styles.cardName}>{pokemon.name}</h3>
+        //     <p className={styles.cardSet}>#{pokemon.id}</p>
+        //     <div className={styles.placeholderImageWrapper}>
+        //       <img
+        //         className={styles.placeholderImage}
+        //         src={pokemon.originalArtwork}
+        //         alt=""
+        //       />
+        //     </div>
+        //   </div>
+        // </div>
+        <NationalDexGridItem pokemon={pokemon} />
       ))}
     </div>
   );
---
+++ src/components/NationalDexGridItem/NationalDexGridItem.tsx
@@ -0,0 +1,62 @@
+import type { Pokemon } from "../NationalDexGrid/NationalDexGrid";
+import styles from "./NationalDexGridItem.module.scss";
+import { useEffect, useRef, useState } from "react";
+
+interface NationalDexGridItemProps {
+  pokemon: Pokemon;
+}
+
+export default function NationalDexGridItem({
+  pokemon,
+}: NationalDexGridItemProps) {
+  const cardRef = useRef<HTMLDivElement | null>(null);
+  const imageRef = useRef<HTMLImageElement | null>(null);
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+  const [dynamicClipPathValue, setDynamicClipPathValue] = useState<number>(0);
+
+  useEffect(() => {
+    if (cardRef.current && imageRef.current) {
+      setDynamicClipPathValue(
+        imageRef.current.clientWidth - cardRef.current.clientWidth,
+      );
+    }
+  }, []);
+
+  const clipPathStyle = {
+    clipPath: `inset(0 ${dynamicClipPathValue}px 0 0)`,
+  };
+
+  const clipPathHoverStyle = {
+    clipPath: `inset(0 ${dynamicClipPathValue}px 0 -50%)`,
+  };
+
+  return (
+    <div
+      key={pokemon.id}
+      className={styles.card}
+      onClick={() => {
+        console.log(`This is #${pokemon.id}, ${pokemon.name} `);
+      }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      ref={cardRef}
+    >
+      <div className={styles.cardInfo}>
+        <h3 className={styles.cardName}>{pokemon.name}</h3>
+        <p className={styles.cardSet}>#{pokemon.id}</p>
+        <div
+          className={styles.placeholderImageWrapper}
+          style={isHovered ? clipPathHoverStyle : clipPathStyle}
+        >
+          <img
+            ref={imageRef}
+            className={[styles.placeholderImage].join(" ")}
+            src={pokemon.originalArtwork}
+            loading="lazy"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
