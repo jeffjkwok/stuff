0dca98c7eede0674c68204b4d6893e6823d11433
Jeff Kwok <jeffjkwok@gmail.com>
2026-02-16 00:31:52 -0800

    clean up server endpoints; remove unused card grid

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,13 +5,30 @@ import "dotenv/config";
 import mockCards from "./data/mock-cards.json";
 import nationalDex from "./data/updatedDex.json";
 import { getCollection, toggleAcquistion } from "./googleSheets";
+import TCGdex, { Query } from "@tcgdex/sdk";
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
 
-const cardMap = new Map(mockCards.map((card: any) => [card.id, card]));
+const tcgdex = new TCGdex("en");
+
+const responseCircularFormatter = (response: any) => {
+  const seen = new WeakSet();
+  return JSON.parse(
+    JSON.stringify(response, (_key, value) => {
+      if (_key == "sdk") return;
+      if (typeof value === "object" && value !== null) {
+        if (seen.has(value)) {
+          return undefined;
+        }
+        seen.add(value);
+      }
+      return value;
+    }),
+  );
+};
 
 app.get("/api/health", (req, res) => {
   res.json({
@@ -19,6 +36,21 @@ app.get("/api/health", (req, res) => {
     message: "Server is running!",
     timestamp: new Date().toISOString(),
   });
+});
+
+app.get("/api/search/:name", async (req, res) => {
+  try {
+    // const response = await tcgdex.card.get('swsh3-136');
+    const response = await tcgdex.card.list(
+      Query.create().contains("name", req.params.name),
+    );
+
+    const formattedData = responseCircularFormatter(response);
+
+    res.json(formattedData);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.get("/api/cards", (req, res) => {
@@ -40,8 +72,6 @@ app.get("/api/collection", async (req, res) => {
 });
 
 app.post("/api/collection/acquired/:dexNumber", async (req, res) => {
-  // UPDATE LOGIC HERE
-  // console.log(req,res)
   try {
     const dexNumber = parseInt(req.params.dexNumber);
     const newStatus = await toggleAcquistion(dexNumber);
@@ -51,65 +81,6 @@ app.post("/api/collection/acquired/:dexNumber", async (req, res) => {
     res
       .status(500)
       .json({ error: "Failed to update acquistion status of pokemon" });
-  }
-});
-
-app.get("/api/pokemon/sprite/:pokeId", async (req, res) => {
-  try {
-    const { pokeId } = req.params;
-    console.log(pokeId);
-
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`);
-
-    if (!response.ok) {
-      return res.status(response.status).json({
-        error: "Pokemon not found",
-      });
-    }
-
-    const data = await response.json();
-    console.log(data);
-    res.json(data.sprites);
-  } catch (err) {
-    console.error("Erro fetching card:", err);
-    res.status(500).json({ error: "Failed to fetch card" });
-  }
-});
-
-app.get("/api/cards/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    await new Promise((resolve) => setTimeout(resolve, 500));
-
-    const card = cardMap.get(id);
-
-    if (card) {
-      res.json(card);
-    } else {
-      res.status(404).json({ error: "Card not found" });
-    }
-
-    // const response = await fetch(
-    //     `https://api.pokemontcg.io/v2/cards/${id}`,
-    //     {
-    //         headers: {
-    //             'X-Api-Key': process.env.POKEMON_TCG_API_KEY || ''
-    //         }
-    //     }
-    // )
-
-    // if (!response.ok) {
-    //     return res.status(response.status).json({
-    //         error: 'Card not found'
-    //     });
-    // }
-
-    // const data = await response.json();
-    // res.json(data.data);
-  } catch (err) {
-    console.error("Erro fetching card:", err);
-    res.status(500).json({ error: "Failed to fetch card" });
   }
 });
 
